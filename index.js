525c04579d1ceb93a7381f845b65c10cda571c25
Genadi <genadi.kozarev@gmail.com>
2021-09-30 23:23:50 +0100

    integrating a leafletjs map - in progress

---
+++ index.js
@@ -0,0 +1,10 @@
+let mymap = L.map('map').setView([51.45564284396053, -2.5885233624922743], 13);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiY3liZXJ3b21iYXQiLCJhIjoiY2t1N2hsMHJvMHJxbDJ5cGF4YzRlMmlvMiJ9.g9RBKwBgBC78nWOWb0YkEg'
+}).addTo(mymap);
