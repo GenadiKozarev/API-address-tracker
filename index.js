ffbe565b0a8a5602e7999d34f1aea43d43d9644d
Genadi <genadi.kozarev@gmail.com>
2021-10-02 19:21:44 +0100

    working on layout

--- index.js
+++ index.js
@@ -8,3 +8,16 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoiY3liZXJ3b21iYXQiLCJhIjoiY2t1N2hsMHJvMHJxbDJ5cGF4YzRlMmlvMiJ9.g9RBKwBgBC78nWOWb0YkEg'
 }).addTo(mymap);
+
+async function getIpGeolocation() {
+    let data = await fetch('https://geo.ipify.org/api/v1?apiKey=at_ja6PEtd9151gqzbREsvcDw4NV7JQr');
+    let jsonData = await data.json();
+
+    console.log(jsonData)
+}
+
+getIpGeolocation()
+
+document.querySelector('.searchButton').addEventListener('click', () => {
+
+})
